6b3383a4e0f3b941b4218da8fa9bb58667dd9017
VseZnal <Vseznal66@gmail.com>
2023-02-21 01:21:34 +0300

    chore: database error

--- libs/errors/errors.go
+++ libs/errors/errors.go
@@ -1,6 +1,8 @@
 package errors
 
 import (
+	"database/sql"
+	"google.golang.org/grpc/status"
 	"log"
 )
 
@@ -14,4 +16,19 @@ func HandleFatalError(err error, message string) {
 	log.Println(err)
 
 	log.Fatal(message)
+}
+
+func HandleDatabaseError(err error) error {
+	log.Println(err)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return status.Error(404, "Record not found")
+	case err == sql.ErrTxDone:
+		return status.Error(500, "Internal server error has occured")
+	case err == sql.ErrConnDone:
+		return status.Error(500, "Internal server error has occured")
+	default:
+		return status.Error(500, "Internal server error has occured")
+	}
 }
